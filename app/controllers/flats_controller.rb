bd933114fc0cb300c7c648acb569518c26f64dda
Tom Brookes <tom.brookes15@gmail.com>
2023-01-26 12:21:29 +0000

    created views and controller actions

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,16 +1,31 @@
 class FlatsController < ApplicationController
-  before_action :set_flat
+  before_action :set_flat, only: [:show, :destroy]
 
-  def index; end
+  def index
+    @flats = Flat.all
+  end
+
   def new; end
+
   def create; end
-  def show; end
+
+  def show
+  end
+
   def edit; end
+
   def update; end
-  def destroy; end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path, status: :see_other
+  end
 
   private
 
-  def set_flat; end
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
   def flat_params; end
 end
